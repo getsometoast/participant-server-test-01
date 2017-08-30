04c61f8f17b5915c98b926716e174f24768576ba
James Lewis <jamesl@thoughtworks.com>
2017-08-30 13:49:27 +1000

    add the actual website which serves an image from a url provided by mongo

--- server.rb
+++ server.rb
@@ -5,5 +5,12 @@ get "/health" do
 	client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'cats')
 	db = client.database
 
-	"healthy"
+  "healthy"
+end
+
+get "/cats" do
+	client = Mongo::Client.new('mongodb://127.0.0.1:27017/test')
+	collection = client[:cats]
+	@model = collection.find({name: 'cat name'}).skip(1).first
+	erb :cats
 end
